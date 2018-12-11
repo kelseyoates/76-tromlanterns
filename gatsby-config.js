65f6ef1d076ef938ec202a268cd045ea980f042a
koates <20542325+kelseyoates@users.noreply.github.com>
2018-12-10 22:36:34 -0500

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,21 @@ module.exports = {
         sv: 6
       },
     },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-130781167-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+
+      },
+    },
 ]
 
 };
