494f45e866e6a86ae22c51966e77ea691b17e12e
Kelsey Oates <kelsey.oates@gmail.com>
2019-03-14 20:07:25 -0400

    switched back

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -14,25 +14,6 @@ const Blog = () => (
       <h2 className="text-center">Research and Insights</h2>
     </div>
 
-
-
-
-        <div className="mx-auto md:w-1/2 sm:w-2/3 xsm:w-1/1 px-9 ">
-          <div className="article-title secondary text-white">
-          <h5>Massachusetts network theory and unconventional political data</h5>
-          </div>
-          <div className="article px-1">
-            <p className="article ">CommonWealth magazine yesterday ran my article about using network theory to understand how cosponsorships in legislation indicate a social structure underlying the state government. Basically, we can take the web of legislators who have cosponsored bills with each other, treat that like a social network, and use some sociological methods to tease out some insights about what that social structure means.</p>
-            <p className="article-link">
-            <a href="https://medium.com/@twolanterns/massachusetts-network-theory-and-unconventional-political-data-b764ad3a9f30"
-              className="px-4 xsm:block sm:inline-block hover:bg-secondary highlight mt-02 font-italic no-underline text-grey-darker">
-        Keep Reading →
-            </a></p>
-          </div>
-        </div>
-
-</div>
-
   
 
     <div className="flex flex-col md:flex-row px-9 pt-5"> 
@@ -53,6 +34,23 @@ const Blog = () => (
           </div>
         </div>
   
+  
+        <div className="mx-auto md:w-1/2 sm:w-2/3 xsm:w-1/1 px-9 ">
+          <div className="article-title secondary text-white">
+          <h5>Massachusetts network theory and unconventional political data</h5>
+          </div>
+          <div className="article px-1">
+            <p className="article ">CommonWealth magazine yesterday ran my article about using network theory to understand how cosponsorships in legislation indicate a social structure underlying the state government. Basically, we can take the web of legislators who have cosponsored bills with each other, treat that like a social network, and use some sociological methods to tease out some insights about what that social structure means.</p>
+            <p className="article-link">
+            <a href="https://medium.com/@twolanterns/massachusetts-network-theory-and-unconventional-political-data-b764ad3a9f30"
+              className="px-4 xsm:block sm:inline-block hover:bg-secondary highlight mt-02 font-italic no-underline text-grey-darker">
+        Keep Reading →
+            </a></p>
+          </div>
+        </div>
+
+</div>
+
 
   </div>
 );
